d22d3b177f0bb06a2d1e1d76de78f9c2d00f55be
chinyao1210 <chinyao1210@gmail.com>
2026-04-25 16:06:44 +0800

    Adjustment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,15 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.chenghakfan.magroupassignment"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "my.edu.utar.mobileappass"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.chenghakfan.magroupassignment"
+        applicationId = "my.edu.utar.mobileappass"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -34,11 +32,17 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
